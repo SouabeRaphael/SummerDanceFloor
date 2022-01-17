22fa9cfc0f2026b84dc85a635df778126825aa19
SouabeRaphael <R.souabe03@gmail.com>
2022-01-17 17:52:48 +0100

    direction la finalisation du design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,60 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
 
   const bouncyCircle = new mojs.Shape({
     parent: ".block",
     shape: "circle",
-    fill: { "#008000": "#0000FF" },
-    radius: { 20: 80 },
+    fill: "#808080",
+    stroke: "#FFFF",
+    strokeWidth: 8,
+    radius: 200,
     duration: 2000,
     isYoyo: true,
     isShowStart: true,
     easing: "elastic.inout",
-    repeat: 1,
+    repeat: 999,
   });
 
-  const mojsPlayer = new MojsPlayer({ add: bouncyCircle });
-  mojsPlayer.play();
+  const form1 = new mojs.Shape({
+    parent: ".form1",
+    shape: "rect",
+    left: 0,
+    top: 0,
+    radius: 20,
+    fill: "#FF0000",
+    stroke: "#FFFF",
+    strokeWidth: 3,
+  }).then({
+    rotate: { 0: 360 },
+    x: { [0]: 750 },
+    y: { [0]: 400 },
+    easing: "linear.none",
+    duration: 1500,
+  });
 
+  const form2 = new mojs.Shape({
+    parent: ".form2",
+    shape: "circle",
+    left: "100vw",
+    top: 0,
+    radius: 20,
+    fill: "#FF0000",
+    stroke: "#FFFF",
+    strokeWidth: 3,
+  }).then({
+    rotate: { 0: 360 },
+    x: { [0]: -800 },
+    y: { [0]: 300 },
+    easing: "linear.none",
+    duration: 1500,
+  });
+
+  // create the timeline
+  const mainTimeline = new mojs.Timeline({});
+  mainTimeline.add(bouncyCircle, form1, form2);
+
+  // add the timeline to the player
+  const mojsPlayer = new MojsPlayer({
+    add: mainTimeline,
+  });
 });
